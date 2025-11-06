6197339ef3714c23243748d243c8e7dc12ddefac
Ангелина Кузнецова <angelkuzk2004k@gmail.com>
2025-11-06 17:47:29 +0700

    Заготовка к 4-ой лабораторной работе

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ import datetime
 app = Flask(__name__)
 
 from lab3 import lab3
+from lab4 import lab4
 
 app.register_blueprint(lab3)
-
+app.register_blueprint(lab4)
 
 access_log = []
 
@@ -363,6 +364,7 @@ def index():
                     <li><a href="/lab1">Первая лабораторная</a></li>
                     <li><a href="/lab2">Вторая лабораторная</a></li>
                     <li><a href="/lab3">Третья лабораторная</a></li>
+                    <li><a href="/lab4">Четвертая лабораторная</a></li>
                 </ul>
             </nav>
         </main>
