0422e316f908aceb3d5938b69441dea10417ddcb
Ангелина Кузнецова <angelkuzk2004k@gmail.com>
2025-11-11 22:37:29 +0700

    Аутентификация

--- lab5.py
+++ lab5.py
@@ -1,10 +1,11 @@
 from flask import Blueprint, render_template, request, make_response, redirect, session
 lab5 = Blueprint('lab5', __name__)
 import psycopg2
+from psycopg2.extras import RealDictCursor
 
 @lab5.route('/lab5/')
 def lab():
-    return render_template('lab5/lab5.html', username='anonymous')
+    return render_template('lab5/lab5.html', username=session.get('login', 'anonymous'))
 
 @lab5.route('/lab5/register', methods=['GET', 'POST'])
 def register():
@@ -46,3 +47,44 @@ def register():
         return render_template('lab5/register.html', error=f'Ошибка подключения к БД: {str(e)}')
     except Exception as e:
         return render_template('lab5/register.html', error=f'Ошибка базы данных: {str(e)}')
+    
+
+@lab5.route('/lab5/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('lab5/login.html')
+    
+    login = request.form.get('login')
+    password = request.form.get('password')
+
+    if not (login and password):
+        return render_template('lab5/login.html', error="Заполните поля")
+    
+    conn = psycopg2.connect(
+            host='127.0.0.1',
+            database='angelina_kuznetsova_knowledge_base',  
+            user='angelina_kuznetsova_knowledge_base',      
+            password='123',
+            port=5432
+        )
+    cur = conn.cursor(cursor_factory = RealDictCursor)
+    
+    cur.execute(f"SELECT * FROM users WHERE login='{login}';")
+    user = cur.fetchone()
+
+    if not user:
+        cur.close()
+        conn.close()
+        return render_template('lab5/login.html',
+                               error='Логин и/или пароль неверны')
+    
+    if user['password'] != password:
+        cur.close()
+        conn.close()
+        return render_template('lab5/login.html',
+                               error='Логин и/или пароль неверны')
+    
+    session['login'] = login
+    cur.close()
+    conn.close()
+    return render_template('lab5/success_login.html', login=login)
