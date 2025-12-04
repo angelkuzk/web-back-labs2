704b445fc565c58221bf305997be2124541eec5c
Ангелина Кузнецова <angelkuzk2004@gmail.com>
2025-12-05 01:28:34 +0700

    исправление

--- lab7.py
+++ lab7.py
@@ -87,15 +87,15 @@ def del_film(id):
 def put_film(id):
     if id < 0 or id >= len(films):
         return jsonify({"error": "Фильм не найден"}), 404
-    
+
     film_data = request.get_json()
-    
+
     if not film_data:
         return jsonify({"error": "Не предоставлены данные для обновления"}), 400
 
-    if film_data['description'] == '':
-        return {'description': 'Заполните описание'}, 400
-    
+    if 'description' not in film_data or not film_data['description'].strip():
+        return jsonify({'description': 'Заполните описание'}), 400
+
     films[id] = film_data
     return jsonify(films[id])
 
@@ -103,15 +103,15 @@ def put_film(id):
 @lab7.route('/lab7/rest-api/films/', methods=['POST'])
 def add_film():
     film_data = request.get_json()
-    
+
     if not film_data:
         return jsonify({"error": "Не предоставлены данные фильма"}), 400
 
 
-    if film_data['description'] == '':
+    if 'description' not in film_data or not film_data['description'].strip():
         return jsonify({'description': 'Заполните описание'}), 400
-    
+
     films.append(film_data)
-    
+
     new_id = len(films) - 1
     return jsonify({"id": new_id}), 201
