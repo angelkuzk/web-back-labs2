210afe1ec585d56203da9514c8a46b21ffa727d0
Ангелина Кузнецова <angelkuzk2004@gmail.com>
2025-12-17 23:08:45 +0700

    Заготовка к 9-ой лабораторной

--- app.py
+++ app.py
@@ -44,6 +44,7 @@ from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
 from lab8 import lab8
+from lab9 import lab9
 
 app.register_blueprint(lab3)
 app.register_blueprint(lab4)
@@ -51,6 +52,7 @@ app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
 app.register_blueprint(lab8, url_prefix='/lab8')
+app.register_blueprint(lab9)
 
 access_log = []
 
@@ -415,6 +417,7 @@ def index():
                     <li><a href="/lab6">Шестая лабораторная</a></li>
                     <li><a href="/lab7">Седьмая лабораторная</a></li>
                     <li><a href="/lab8">Восьмая лабораторная</a></li>
+                    <li><a href="/lab9">Девятая лабораторная</a></li>
                 </ul>
             </nav>
         </main>
